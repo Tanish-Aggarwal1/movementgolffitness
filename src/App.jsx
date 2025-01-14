34a9c02313e6d975207430753b2dc2c5a9e9d2cb
Harsheary <harsheary2022005@gmail.com>
2025-01-14 13:15:51 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl bg-red-300 rounded-xl'>movement Golf fitness</h1>
+    </>
+  )
+}
+
+export default App
